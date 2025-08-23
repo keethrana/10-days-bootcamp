dcd2791901e17e6e1afecb9df50d7df97ae1b01b
RKeerthana09 <keerthanaraja0905@gmail.com>
2025-08-23 09:29:02 +0530

    first commit

--- day9/landing.js
+++ day9/landing.js
@@ -1,8 +1,8 @@
 const con= document.getElementById('container');
 const arr=["https://imageio.forbes.com/specials-images/imageserve/67359a8c6331660e9efb8bac/0x0.jpg?format=jpg&crop=1920,1080,x0,y0,safe&height=600&width=1200&fit=bounds",
-    "https://assets.gqindia.com/photos/67c033f43f0f13336ec24167/16:9/w_2560%2Cc_limit/bollywood-movies-on-netflix-amazon-prime-video-hotstar-quarantine.jpg",
-    "https://static.toiimg.com/thumb/msid-117249524,width-1070,height-580,imgsize-77990,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
     "",
+    "https://static.toiimg.com/thumb/msid-117249524,width-1070,height-580,imgsize-77990,resizemode-75,overlay-toi_sw,pt-32,y_pad-40/photo.jpg",
+    "https://www.hollywoodreporter.com/wp-content/uploads/2025/02/The-Ba___ds-of-Bollywood_Netflix_857616_v001.0001.jpg?w=1296&h=730&crop=1",
     
     /* "./assest/27.jpg", */
 
